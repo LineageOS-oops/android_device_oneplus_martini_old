02e739560700e5b4667b5b818465b888754e5926
sergoops <sergoopsmoskal@gmail.com>
2024-03-15 20:19:39 +0300

    Revert "sm8350-common: Copy configs and scripts for yupik"
    This reverts commit 82005af97269024d3ad677974ed6d314dd0e49f0.

--- extract-files.sh
+++ extract-files.sh
@@ -75,7 +75,7 @@ function blob_fixup() {
         vendor/bin/init.kernel.post_boot-lahaina.sh)
             sed -i "s/uag/schedutil/" "${2}"
             ;;
-        vendor/etc/media_*/video_system_specs.json)
+        vendor/etc/media_lahaina/video_system_specs.json)
             sed -i "/max_retry_alloc_output_timeout/ s/1000/0/" "${2}"
             sed -i "/pipelining/ s/^/\/\//" "${2}"
             sed -i "/liboplusvppfilter.so/ s/^/\/\//" "${2}"
