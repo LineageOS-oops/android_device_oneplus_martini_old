750b173a258ca442988627faa05f245251196f5e
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    martini: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- device.mk
+++ device.mk
@@ -28,9 +28,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# APEX
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
