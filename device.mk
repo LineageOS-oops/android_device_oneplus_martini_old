5bb01a40361dd97f9860e697a24631b8c8c4d13a
sergoops <sergoopsmoskal@gmail.com>
2024-03-31 11:34:18 +0300

    martini: Build GAPPS inline
    * It's have some patches in fw_base that cause crashes without gapps
    * It's switched to erofs completly, so it's impossible to sideload gapps manually
    
    * MindTheGapps is in use

--- device.mk
+++ device.mk
@@ -226,6 +226,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# GAPPS (MindTheGapps)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
 # Google Photos
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
